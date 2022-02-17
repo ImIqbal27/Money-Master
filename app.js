88a49dac841816d8d144fa63e4ea54f88da6d353
unknown <iamhasan9501@gmail.com>
2022-02-17 21:33:12 +0600

    design chngd

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-
-
-
-
 /////////////////////////// function /////////////////////
 function getInputValue(inputId) {
     const inputField = document.getElementById(inputId);
     const inputAmountText = inputField.value;
     const inputAmount = parseFloat(inputAmountText);
-    return inputAmount;
+
+    if (typeof (inputAmount) != 'number' && typeof (inputAmount) == 'string' && inputAmount < 0) {
+        const a = document.getElementById('income-error');
+        a.innerText = 'Invalid input: Only positive number is acceptable! ';
+
+
+    }
+    else {
+        return inputAmount;
+
+    }
+
+
+
+
 };
 
 function displayNumbers(totalFieldId, amount) {
@@ -17,32 +27,39 @@ function displayNumbers(totalFieldId, amount) {
     totalElement.innerText = amount;
 }
 ///////////////////////////////////////////////////////////
-
-
-
-
-
 /////////////////// calculate  button handle ////////////
+///////////////////////////////////////////////////////////
 document.getElementById('calculate-btn').addEventListener('click', function () {
     const income = getInputValue('input-income');
+
     const food = getInputValue('food-input');
     const rent = getInputValue('rent-input');
     const cloths = getInputValue('cloths-input');
     const totalCost = food + rent + cloths;
-    const totalCostDisplay = displayNumbers('display-total-cost', totalCost);
 
-    const balance = income - totalCost;
-    const balanceDisplay = displayNumbers('display-balance', balance)
+
+    if (income > totalCost) {
+        const balance = income - totalCost;
+        const balanceDisplay = displayNumbers('display-balance', balance)
+        const totalCostDisplay = displayNumbers('display-total-cost', totalCost);
+
+    }
+    else {
+        alert('Total expenses can`t be more then your Income !');
+    }
+
+
 });
 
 ////////////////  save button handle  /////////////////////
 document.getElementById('save-btn').addEventListener('click', function () {
-    const percentageNumber = getInputValue('save-input');
-    const income = getInputValue('input-income');
-    const savingAmount = (income * percentageNumber) / 100;
-    const savingAmountDisplay = displayNumbers('display-saving-amount', savingAmount);
 
-    // const income = getInputValue('input-income');
+    const income = getInputValue('input-income');
+    const percentageNumber = getInputValue('save-input');
+
+
+    const savingAmount = (income * percentageNumber) / 100;
+
     const food = getInputValue('food-input');
     const rent = getInputValue('rent-input');
     const cloths = getInputValue('cloths-input');
@@ -50,9 +67,19 @@ document.getElementById('save-btn').addEventListener('click', function () {
 
     const balance = income - totalCost;
 
+    if (savingAmount <= balance) {
+        const savingAmountDisplay = displayNumbers('display-saving-amount', savingAmount);
+        const remainingBalance = balance - savingAmount;
+        const remainingBalanceDisplay = displayNumbers('display-remaining-balance', remainingBalance)
 
-    const remainingBalance = balance - savingAmount;
-    const remainingBalanceDisplay = displayNumbers('display-remaining-balance', remainingBalance)
+    }
+    else {
+        const a = document.getElementById('saving-error');
+        a.innerText = 'Low balance ';
+    }
+
+
+
 
 });
 
